97755057d09988742746f3622ee76e6f8abcee95
Matt Schroder <m3schroder@gmail.com>
2020-05-28 15:59:40 +0000

    Added case insensitivity and fixed duplicates in the AR15 site

--- main.py
+++ main.py
@@ -9,32 +9,50 @@ app = Flask(__name__)
 
 @app.route('/')
 def webScrape():
-        keyWords = open("keywords.txt").read().splitlines() 
         
-        df = pd.DataFrame(columns = ['Keyword','Title','URL'])
+        keywords = open('keywords.txt').read().splitlines()
+
+        df = pd.DataFrame(columns=['Keyword', 'URL', 'Title', 'Info'])
         
-        #AR15.com
-        data = requests.get('https://urldefense.com/v3/__https://www.ar15.com/forums/Equipment-Exchange/AR15-Parts-New-Lower-Receiver/125/__;!!Bbg-OcCDlOs!T6SPqMDtsVY4Vk39ZzosABP95RtKZ_FkgWA9Fczc7IfsrSGT315NdhcvHQtW1JwZRjyAkA$ ')
+        # New Lower AR15
+        data = requests.get('https://www.ar15.com/forums/Equipment-Exchange/AR15-Parts-New-Lower-Receiver/125/')
         soup = BeautifulSoup(data.text, 'html.parser')
-        links = soup.find_all('a')
-        ar15Site = 'https://www.ar15.com'
-        
-        for x in keyWords:
+        links = soup.find_all('div', {'class':'large-6 medium-9 small-12 columns'})
+        prefix = "https://www.ar15.com"
+
+        for x in keywords:     
+                x = x.upper()
                 for l in links:
-                        href = l.get('href')
-                        title = l.get('title')
-                        if x in href and title != None:
-                                df.loc[df.shape[0]+1, 'URL'] = ar15Site+href
-                                df.loc[df.shape[0], 'Title'] = str(title)
-                                df.loc[df.shape[0], 'Keyword'] = str(x)
+                        href = l.find_all('a')[1].get('href')
+                        info = l.find_all('a')[1].get('title')
+                        title = l.find_all('a')[1].getText()
+                        if x in href.upper():
+                                df = df.append(pd.DataFrame([[x, prefix+href, title, info]],columns=['Keyword', 'URL', 'Title', 'Info']))
+
+        # M4 site parts 
+        data = requests.get('https://www.m4carbine.net/forumdisplay.php?111-AR-Parts-amp-Furniture-(EE)')
+        soup = BeautifulSoup(data.text, 'html.parser')
+        links = soup.find_all('div', {'class':'threadinfo'})
+        prefix = "https://www.m4carbine.net"
         
-        pd.set_option('colheader_justify', 'center')   # FOR TABLE <th>
+        for x in keywords:
+                x = x.upper()   
+                for l in links:
+                        title = l.find('a',{'class':'title'}).getText()
+                        info = l.get('title')
+                        href = l.find('a',{'class':'title'}).get('href')
+                        if x in href.upper():
+                                df = df.append(pd.DataFrame([[x, prefix+href, title, info]],columns=['Keyword', 'URL', 'Title', 'Info']))
+
+        # Export data to Excel
+        pd.set_option('colheader_justify', 'center')
         
         html_string = '''
                 <html>
                 <head><title>HTML Pandas Dataframe with CSS</title></head><link rel="stylesheet" type="text/css" href="/static/df_style.css?q=
                 '''+str(random.randint(0,100))+'''"/><body> {table}</body></html>.'''
         
+        df = df.reset_index(drop = True)
         with open('templates/table.html', 'w') as f:
                 f.write(html_string.format(table=df.to_html(classes='mystyle', justify = 'center', render_links = True)))
         
